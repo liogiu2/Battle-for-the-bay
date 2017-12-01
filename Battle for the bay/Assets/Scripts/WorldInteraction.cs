9d9a91529305290fba35f0fb4c8d536bf9bad1ee
teddimagg <teddi@menn.is>
2017-12-01 17:50:32 +0000

    player movement mechanics and minor level design

---
+++ Battle for the bay/Assets/Scripts/WorldInteraction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WorldInteraction : MonoBehaviour {
+    NavMeshAgent playerAgent;
+
+    private void Start() {
+        playerAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update () {
+        if (Input.GetMouseButtonDown(1) && !UnityEngine.EventSystems.EventSystem.current.IsPointerOverGameObject()) {
+            getIntercation();
+        }
+	}
+
+    void getIntercation() {
+        Ray interactionRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit interactionInfo;
+        if(Physics.Raycast(interactionRay, out interactionInfo, Mathf.Infinity))
+        {
+            GameObject interactedObject = interactionInfo.collider.gameObject;
+            if(interactedObject.tag == "Interactive Object") {
+                Debug.Log("Interactable interacted.");
+            } else {
+                playerAgent.destination = interactionInfo.point;
+
+            }
+        }
+    }
+}
---
+++ Battle for the bay/Assets/followingCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followingCamera : MonoBehaviour {
+    public float cameraY = 12;
+    public float cameraZ = 5;
+    private Camera mainCamera;
+    private GameObject player;
+
+    // Use this for initialization
+    void Start () {
+        mainCamera = GetComponent<Camera>();
+        player = GameObject.Find("Player");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 playerInfo = player.transform.transform.position;
+        Debug.Log(mainCamera.transform.rotation);
+        mainCamera.transform.position = new Vector3(playerInfo.x, playerInfo.y + cameraY, playerInfo.z - cameraZ);
+    }
+}
