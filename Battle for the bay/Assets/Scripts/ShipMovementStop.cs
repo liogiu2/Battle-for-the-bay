3830075f86e1b9c70bbde2c8be7b3552ee8492d4
Michelangelo Diamanti <michelangelo.diamanti@gmail.com>
2017-12-02 14:32:48 +0000

    pathFinding with navcontroller

---
+++ Battle for the bay/Assets/Scripts/ShipMovementStop.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipMovementStop : MonoBehaviour {
+
+	public float rotoSpeed;
+	public ShipMovement shipMovement;
+	public SpriteRenderer cursorSprite; 
+
+	// Use this for initialization
+	void Start () {
+		cursorSprite.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(Vector3.forward * rotoSpeed * Time.deltaTime);
+	}
+
+	void OnTriggerEnter(Collider other){
+		if(other.tag == shipMovement.tag){
+			if(shipMovement.moving == true){
+				shipMovement.moving = false;
+				cursorSprite.enabled = false;
+			}
+		}
+	}
+}
