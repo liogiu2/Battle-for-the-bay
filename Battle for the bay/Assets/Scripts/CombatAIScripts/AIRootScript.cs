0b91370390a914f9afd5e96ecf8a0b723689d52a
liogiu2 <mori.giulio.94@gmail.com>
2017-12-02 15:04:13 +0000

    Start making combat system ai

---
+++ Battle for the bay/Assets/Scripts/CombatAIScripts/AIRootScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIRootScript : MonoBehaviour {
+
+	public enum STATE
+	{
+		Idle, 
+		Combat
+	}
+	public STATE currentState;
+	public List<AIRootScript> detected, enemies;
+	// Use this for initialization
+	void Start () {
+		ChangeState(STATE.Idle);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void ChangeState(STATE state){
+		currentState = state;
+	}
+}
---
+++ Battle for the bay/Assets/Scripts/CombatAIScripts/DetectionScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectionScript : MonoBehaviour
+{
+
+    public AIRootScript rootScript;
+    // Use this for initialization
+    void Start()
+    {
+        rootScript = gameObject.GetComponentInParent<AIRootScript>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //clear and update che enemy list
+        if (rootScript.enemies.Count > 0)
+        {
+            rootScript.enemies.Clear();
+        }
+
+        foreach (AIRootScript detectedObject in rootScript.detected)
+        {
+            if (detectedObject == null)
+            {
+                rootScript.detected.Remove(detectedObject);
+            }
+
+            if (detectedObject.tag == "ship")
+            {
+                rootScript.enemies.Add(detectedObject);
+            }
+        }
+
+        //Change the state
+        if (rootScript.enemies.Count > 0)
+        {
+            rootScript.ChangeState(AIRootScript.STATE.Combat);
+        }
+        else
+        {
+            rootScript.ChangeState(AIRootScript.STATE.Idle);
+        }
+    }
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag != "Terrain" && collider.tag != "obstacle" && collider.tag != "Untagged")
+        {
+            rootScript.detected.Add(collider.GetComponent<AIRootScript>());
+        }
+    }
+
+    public void OnTriggerExit(Collider collider)
+    {
+        rootScript.detected.Remove(collider.GetComponent<AIRootScript>());
+    }
+}
