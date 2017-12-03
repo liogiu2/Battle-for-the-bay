cf865f1bbab898f61cd141b456f5c810b5fdc664
teddimagg <teddi@menn.is>
2017-12-03 03:07:59 +0000

    ship animation and game main menu

---
+++ Battle for the bay/Assets/Scripts/GameMode.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameMode : MonoBehaviour {
+
+	public void NewGameBtn(string newGameLevel) {
+        SceneManager.LoadScene(newGameLevel);
+    }
+}
