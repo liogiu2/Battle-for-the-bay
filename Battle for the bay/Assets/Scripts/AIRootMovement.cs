f58c62e936f8d3f57069f9c97f756b87cf9a6668
Michelangelo Diamanti <michelangelo.diamanti@gmail.com>
2017-12-04 16:20:58 +0000

    [Orbit Behaviour] When the player is in attack range the enemy starts orbiting around it in random intervals
    TO DO:
    - need to fix the rotation of the enemy towards the center of the player's
      ship

--- Battle for the bay/Assets/Scripts/AIRootMovement.cs
+++ Battle for the bay/Assets/Scripts/AIRootMovement.cs
@@ -9,22 +9,44 @@ public class AIRootMovement : MonoBehaviour
     public enum STATE
     {
         Idle,
+        OrbitAround,
         Wander,
         Chase,
     }
     public STATE currentState;
 
+    // WANDERING BEHAVIOUR
     public float wanderRadius;
     public float wanderTimer;
 
     private NavMeshAgent agent;
     private float timer;
 
+
+    // CHASE BEHAVIOUR
     private GameObject target;
+
+    // IDLE BEHAVIOUR
+    public float maxIdleTime;
+    private float idleTime;
+    private float idleTimer;
+
+    // ORBIT BEHAVIOUR
+    private Vector3 axis = Vector3.up;
+    private Vector3 desiredPosition;
+    private Transform desiredRotation;
+    public float orbitRadius;
+    public float orbitRadiusSpeed;
+    public float rotationSpeed;
+    public float maxOrbitTime;
+    private float orbitTime;
+    private float orbitTimer;
 
     // Use this for initialization
     void Start()
     {
+        orbitRadius = 2.0f;
+        orbitTimer = 0f;
         // wanderTimer = 10f;
         // wanderRadius = 10f;
         agent = gameObject.GetComponent<NavMeshAgent>();
@@ -47,6 +69,9 @@ public class AIRootMovement : MonoBehaviour
             case STATE.Idle:
                 Idle();
                 return;
+            case STATE.OrbitAround:
+                OrbitAround();
+                return;
             case STATE.Wander:
                 Wander();
                 return;
@@ -67,7 +92,36 @@ public class AIRootMovement : MonoBehaviour
     private void Idle()
     {
         agent.isStopped = true;
+
+        if (idleTimer >= idleTime)
+        {
+            orbitTime = Random.Range( 0f, maxOrbitTime );
+            idleTimer = 0f;
+            ChangeState(STATE.OrbitAround);
+        }
+        idleTimer += Time.deltaTime;
+
     }
+    private void OrbitAround()
+    {
+        agent.isStopped = true;
+
+        transform.RotateAround(target.transform.position, axis, rotationSpeed * Time.deltaTime);
+        //desiredRotation = new GameObject().transform;
+        desiredRotation.LookAt(target.transform.position);
+        transform.rotation = Quaternion.Lerp(transform.rotation, desiredRotation.rotation, Time.deltaTime);
+        //desiredPosition = (transform.position - target.transform.position).normalized * orbitRadius + target.transform.position;
+        //transform.position = Vector3.MoveTowards(transform.position, desiredPosition, Time.deltaTime * orbitRadiusSpeed);
+
+        if(orbitTimer >= orbitTime)
+        {
+            idleTime = Random.Range( 0f, maxIdleTime );
+            orbitTimer = 0f;
+            ChangeState(STATE.Idle);
+        }
+        orbitTimer += Time.deltaTime;
+    }
+
     private void Wander()
     {
 
