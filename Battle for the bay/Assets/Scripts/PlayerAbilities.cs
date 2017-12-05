9bd8429cb6dd89a184a97995d73ccf1c6428c5a1
Theódór Magnússon <teddi@menn.is>
2017-12-05 15:21:29 +0000

    merged player abilities logic

--- Battle for the bay/Assets/Scripts/PlayerAbilities.cs
+++ Battle for the bay/Assets/Scripts/PlayerAbilities.cs
@@ -9,14 +9,21 @@ public class PlayerAbilities : MonoBehaviour {
     SpriteRenderer areaSprite;
     SpriteRenderer rangeSprite;
 
+    public GameObject lineShotAim;
+    public GameObject bulletPrefab;
+    private GameObject player;
+    private MoveInput moveInput;
+
     void Start() {
         areaSprite = areaPointer.GetComponent<SpriteRenderer>();
         rangeSprite = rangePointer.GetComponent<SpriteRenderer>();
+
+        lineShotAim.SetActive(false);
+        player = GameObject.Find("Player").gameObject;
+        moveInput = player.GetComponent<MoveInput>();
     }
 
     void Update() {
-        areaSprite.enabled = false;
-        rangeSprite.enabled = false;
         // Toggling aim mode
         if (Input.GetKeyDown("q"))
         {
@@ -25,6 +32,8 @@ public class PlayerAbilities : MonoBehaviour {
         else if (Input.GetKeyDown("w"))
         {
             aimMode = (aimMode == 0) ? 2 : 0;
+            lineShotAim.SetActive(true);
+            moveInput.enabled = false;
         }
         else if (Input.GetKeyDown("e"))
         {
@@ -45,9 +54,17 @@ public class PlayerAbilities : MonoBehaviour {
 
                         break;
                     case 2:
-                        // Range mode
-                        rangePointer.position = this.gameObject.transform.position;
+                        // Range 
                         rangeSprite.enabled = true;
+
+                        Debug.DrawLine(ray.origin, hit.point);
+                        Vector3 position = new Vector3(hit.point.x, lineShotAim.transform.position.y, hit.point.z);
+                        lineShotAim.transform.LookAt(position);
+                        if (Input.GetMouseButtonDown(0))
+                        {
+                            FireTowards(position);
+                        }
+                        
                         break;
                     case 3:
                         // Splash Area mode
@@ -58,6 +75,35 @@ public class PlayerAbilities : MonoBehaviour {
                         break;
                 }
             }
+        } else
+        {
+            areaSprite.enabled = false;
+            rangeSprite.enabled = false;
+            lineShotAim.SetActive(false);
+            moveInput.enabled = true;
+
         }
+    }
+
+    private void FireTowards(Vector3 Target)
+    {
+        Vector3 bulletPosition = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+
+        //CREATE THE BULLET
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletPosition,
+            //Quaternion.Euler(-10, transform.rotation.y - 90, 0));
+            Quaternion.identity);
+        Destroy(bullet, 1.0f);
+
+        bullet.GetComponent<BulletsBehaviour>().GeneratedTag = gameObject.tag;
+        //COLOR THE BULLET
+        bullet.GetComponent<MeshRenderer>().material.color = Color.black;
+
+        //GIVE INITIAL VELOCITY TO THE BULLET
+        //bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 12;
+        bullet.GetComponent<Rigidbody>().velocity = (Target - bulletPosition).normalized * 10f;
+
     }
 }
