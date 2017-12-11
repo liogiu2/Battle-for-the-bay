e7dfff48cfd0e44000fcf41fad8ff183fa67613e
Matteo Altobelli <matteo.altobelli.92@gmail.com>
2017-12-11 15:21:10 +0000

    menu and HUD

---
+++ Battle for the bay/Assets/Scripts/MenuHandler.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuHandler : MonoBehaviour {
+
+    private bool isOpen;
+    private GameObject menu;
+    private GameObject hud;
+    private float health;
+    private GameObject playerBase;
+    private float initHealth;
+
+    public Image statusPlayer;
+    public Slider[] volumeSliders;
+    public Toggle[] resolutionToggles;
+    public Toggle fullscreenToggle;
+    public int[] screenWidths;
+    int activeScreenResIndex;
+
+    // Use this for initialization
+    void Start () {
+        menu = GameObject.Find("Menu").gameObject;
+        hud = GameObject.Find("HUD").gameObject;
+        menu.SetActive(false);
+        isOpen = false;
+
+        //PLAYER PREFERENCES
+        activeScreenResIndex = PlayerPrefs.GetInt("screen res index");
+        bool isFullscreen = (PlayerPrefs.GetInt("fullscreen") == 1) ? true : false;
+
+        //volumeSliders[0].value = AudioManager.instance.masterVolumePercent;
+        //volumeSliders[1].value = AudioManager.instance.musicVolumePercent;
+        //volumeSliders[2].value = AudioManager.instance.sfxVolumePercent;
+
+        for (int i = 0; i < resolutionToggles.Length; i++)
+        {
+            resolutionToggles[i].isOn = i == activeScreenResIndex;
+        }
+
+        fullscreenToggle.isOn = isFullscreen;
+
+        playerBase = GameObject.Find("Base").gameObject;
+        health = 0;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (isOpen)
+            {
+                isOpen = false;
+                menu.SetActive(false);
+                hud.SetActive(true);
+            }
+            else
+            {
+                isOpen = true;
+                menu.SetActive(true);
+                hud.SetActive(false);
+            }
+        }
+
+        //UPDATE STATUS BASE INDICATOR 
+        health = playerBase.GetComponent<structureHealth>().health;
+        initHealth = playerBase.GetComponent<structureHealth>().initHealth;
+        statusPlayer.fillAmount = health / initHealth;
+    }
+
+    public void SetScreenResolution(int i)
+    {
+        if (resolutionToggles[i].isOn)
+        {
+            activeScreenResIndex = i;
+            float aspectRatio = 16 / 9f;
+            Screen.SetResolution(screenWidths[i], (int)(screenWidths[i] / aspectRatio), false);
+            PlayerPrefs.SetInt("screen res index", activeScreenResIndex);
+            PlayerPrefs.Save();
+        }
+    }
+
+    public void SetMusicVolume(float value)
+    {
+        //AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Music);
+    }
+
+    public void SetSFXVolume(float value)
+    {
+        //AudioManager.instance.SetVolume(value, AudioManager.AudioChannel.Sfx);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        for (int i = 0; i < resolutionToggles.Length; i++)
+        {
+            resolutionToggles[i].interactable = !isFullscreen;
+        }
+
+        if (isFullscreen)
+        {
+            Resolution[] allResolutions = Screen.resolutions;
+            Resolution maxResolution = allResolutions[allResolutions.Length - 1];
+            Screen.SetResolution(maxResolution.width, maxResolution.height, true);
+        }
+        else
+        {
+            SetScreenResolution(activeScreenResIndex);
+        }
+
+        PlayerPrefs.SetInt("fullscreen", ((isFullscreen) ? 1 : 0));
+        PlayerPrefs.Save();
+    }
+}
