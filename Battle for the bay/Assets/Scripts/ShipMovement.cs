bec70396acfb1ef6b2d937ff75856df8544413f7
Michelangelo Diamanti <michelangelo.diamanti@gmail.com>
2017-12-17 19:58:19 +0000

    removed water effect

--- Battle for the bay/Assets/Scripts/ShipMovement.cs
+++ Battle for the bay/Assets/Scripts/ShipMovement.cs
@@ -3,36 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class ShipMovement : MonoBehaviour
-{
+public class ShipMovement : MonoBehaviour {
 
-    public bool moving;
-    public NavMeshAgent agent;
-    public Transform pointer;
-    public GameObject movingEffect;
+	public bool moving;
+	public NavMeshAgent agent;
+	public Transform pointer;
 
-    // Use this for initialization
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (moving)
-        {
-            agent.SetDestination(pointer.position);
-            Invoke("EnableMovingEffect", 0.2f);
-            agent.isStopped = false;
-        }
-        else
-        {
-            movingEffect.SetActive(false);
-        }
-    }
-
-	void EnableMovingEffect(){
-		movingEffect.SetActive(true);
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(moving)
+		{
+			agent.SetDestination(pointer.position);
+			agent.isStopped = false;
+		}
 	}
 }
